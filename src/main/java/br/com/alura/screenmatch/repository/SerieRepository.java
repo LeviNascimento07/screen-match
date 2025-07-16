958dbf7cfe5f2221919a1b5650b1ff3d34b84901
ZNitr0 <levidonascimento2410@gmail.com>
2025-07-16 20:55:24 -0300

    atualizando series

--- src/main/java/br/com/alura/screenmatch/repository/SerieRepository.java
+++ src/main/java/br/com/alura/screenmatch/repository/SerieRepository.java
@@ -22,6 +22,17 @@ public interface SerieRepository extends JpaRepository<Serie, Long> {
    @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE e.titulo ILIKE %:trechoEpisodio%")
    List<Episodio> episodiosPorTrecho(String trechoEpisodio);
 
+   List<Serie> findTop5ByOrderByAvaliacaoDesc();
 
+   @Query("select s from Serie s " +
+           "join s.episodios e  " +
+           "group by s " +
+           "order by max(e.dataEpisodio) desc limit 5 ")
+   List<Serie> lancamentoMaisRecente();
 
+   @Query("SELECT e FROM Serie s JOIN s.episodios e WHERE s.id = :id AND e.temporada = :numero")
+   List<Episodio> obterEpisodiosPorTemporada(Long id, Long numero);
 }
+
+
+
